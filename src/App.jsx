d5f4960b9cd4eebfcf6a73e2226cc6adadd3be47
Hammad Azam <mhammadazam1@gmail.com>
2025-02-21 21:57:40 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react' // HOOKS COME FROM HERE
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hammad Azam</h1>
+      <h2>Counter value: {counter}</h2>
+      <button>Add Value</button> <br />
+      <button>Decrease Value</button>
+    </>
+  )
+}
+
+export default App
